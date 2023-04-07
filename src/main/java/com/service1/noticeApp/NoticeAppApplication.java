a8d4b89becd9349134b26fcd361e200e73abe1cd
David0226 <sorwd2001@naver.com>
2023-04-07 14:23:42 +0900

    #0 start

---
+++ src/main/java/com/service1/noticeApp/NoticeAppApplication.java
@@ -0,0 +1,13 @@
+package com.service1.noticeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/service1/noticeApp/NoticeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service1.noticeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
